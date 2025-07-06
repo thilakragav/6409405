1dae7008bce06eeb027a09ead1775f9ac43ce168
Thilakragav <150499687+thilakragav@users.noreply.github.com>
2025-07-06 11:39:18 +0530

    Week 3 Task completed

---
+++ Week 3_6409405/Spring Core and Maven/Week 3_Exercise 4 Creating and Configuring a Maven Project/Creating configuring Maven Project/src/main/java/com/library1/MainApp.java
@@ -0,0 +1,11 @@
+package com.library1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Library Management App is running...");
+        
+    }
+}
---
+++ Week 3_6409405/Spring-data-jpa-handson/Spring Data JPA - Quick Example/Spring Data JPA - Quick Example/src/main/java/com/thilak/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,26 @@
+
+package com.thilak.ormlearn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.thilak.ormlearn.model.Country;
+import com.thilak.ormlearn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Country country = countryService.findCountryByCode("IN");
+        System.out.println(country);
+    }
+}
